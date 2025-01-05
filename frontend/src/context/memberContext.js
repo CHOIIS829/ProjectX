4a01e0042c4d4a18383f14e1579f67bbdc877db5
tromxx <93820349+tromxx@users.noreply.github.com>
2025-01-05 16:27:35 +0900

    Update memberContext.js

--- frontend/src/context/memberContext.js
+++ frontend/src/context/memberContext.js
@@ -7,6 +7,9 @@ const MemberContext = createContext({
         memberName: "",
         email: "",
     },
+    // PK 값 으로 회원 조회 할건지 아니면 아아디(unique 값이라) 
+    // 로그인 하면 ->  저 값들이 들어옴
+    // 로그인 판단 여부 가 안됨
     isLoggedIn: false,
     setLoggedUser: () => {},
     setLoggedOut: () => {}
