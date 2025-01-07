83820aa86061e82af167e8b81b47fae54e2a7d72
CHOIIS829 <cth7097@gmail.com>
2025-01-07 16:30:18 +0900

    up

--- backend/src/main/java/com/insu/backend/project/request/ProjectSearchRequest.java
+++ backend/src/main/java/com/insu/backend/project/request/ProjectSearchRequest.java
@@ -1,17 +1,18 @@
 package com.insu.backend.project.request;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class ProjectSearchRequest {
 
-    private Integer page = 1;
-    private Integer size = 10;
-
-    private String category = "all";
-    private String isClosed = "all";
+    private Integer page;
+    private Integer size;
+    private String category;
+    private String isClosed;
     private String keyword;
     private String author;
 
